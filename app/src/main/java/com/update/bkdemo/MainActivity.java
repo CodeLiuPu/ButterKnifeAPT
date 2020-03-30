9f8734c5469d5b2e7969f40a88d2392488a32736
liupu <575866447@qq.com>
2020-03-30 12:31:29 +0800

    ButterKnife.bind

--- app/src/main/java/com/update/bkdemo/MainActivity.java
+++ app/src/main/java/com/update/bkdemo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.update.apt_annotation.BindView;
+import com.update.apt_core.ButterKnife;
 
 /**
  * @author liupu
@@ -19,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        tvContent.setText("Heheheehhe");
     }
 }
--- apt_core/src/main/java/com/update/apt_core/ButterKnife.java
+++ apt_core/src/main/java/com/update/apt_core/ButterKnife.java
@@ -1,5 +1,9 @@
 package com.update.apt_core;
 
+import android.app.Activity;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Created on 2020/03/30
  * Description:
@@ -7,4 +11,15 @@ package com.update.apt_core;
  * @author liupu
  */
 public class ButterKnife {
+
+    public static void bind(Activity activity) {
+        try {
+            Class bindClazz =
+                    Class.forName(activity.getClass().getName() + "_ViewBinding");
+            Constructor bindConstructor = bindClazz.getDeclaredConstructor(activity.getClass());
+            bindConstructor.newInstance(activity);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
